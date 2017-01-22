b20c85deb74a50d9097987518161948f88a16004
Fadi Qassem <fadi.qassem@precor.com>
2017-01-21 23:43:51 -0800

    Fixing POST undefined body

--- part4/apiServer.js
+++ part4/apiServer.js
@@ -1,16 +1,48 @@
 import express from 'express';
+import bcrypt from 'bcrypt';
+import bodyParser from 'body-parser';
+
+const users = { 'tomjones': { username: 'tomjones', password: bcrypt.hashSync('password', 10), email: 'tom@jones.com', firstName: 'Tom', lastName: 'Jones', role: 'USER' } };
 
 const api = express();
 const router = express.Router();
+const urlencodedParser = bodyParser.urlencoded({ extended: true });
 
 //Sample GET API
-router.route('/testGet').get((req, res) => {
+router.get('/testGet', (req, res) => {
     res.send('Hello!');
 });
 
-//Sample POST API
-router.route('/signIn').post((req, res) => {
-    res.sendStatus(200);
+router.post('/signIn', urlencodedParser, (req, res) => {
+    const credentials = req.body;
+
+    if(!credentials) {
+        return res.sendStatus(400);
+    }
+
+    if(!credentials.username || !credentials.password) {
+        return res.status(400).send('Missing username or password');
+    }
+
+    const user = users[credentials.username];
+    if(user === undefined) {
+        return res.status(400).send('User not found');
+    }
+
+    bcrypt.compare(credentials.password, user.password, (err, bcryptResult) => {
+        if (err) throw err;
+        if(bcryptResult) {
+            const truncatedUser = {
+                username: user.username,
+                email: user.email,
+                firstName: user.firstName,
+                lastName: user.lastName,
+                role: user.role
+            };
+            return res.status(200).send(truncatedUser);
+        }
+        return res.status(401).send({ password: 'Username or password was invalid' });
+    });
 });
 
 //CORS
